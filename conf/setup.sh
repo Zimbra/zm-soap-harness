0adcacce668598a27bcfa6ed1710ab82094f9075
SYNBUF\swapnil.pingle <swapnil.pingle@synacor.com>
2018-07-16 16:37:33 +0530

    ZCS-5593 : Added validations/log message for the steps

--- conf/setup.sh
+++ conf/setup.sh
@@ -1,11 +1,16 @@
-echo "Copying commercial cert for installation"
-mkdir /tmp/commercial
-cp /opt/qa/soapvalidator/conf/commercial/* /opt/zimbra/ssl/zimbra/commercial/
-cp /opt/qa/soapvalidator/conf/commercial/* /tmp/commercial/
+if [ "$(whoami)" != "root" ]; then
+        echo "Script must be run as user: root"
+        exit -1
+fi
 echo "Enabling unauth pings"
 su - zimbra -c "zmlocalconfig -e allow_unauthed_ping=true"
 echo "Installing and activating license"
-su - zimbra -c "zmlicense -i /tmp/regular.xml|zmlicense -a"
+wget --no-check-certificate --no-proxy -O /tmp/regular.xml "http://zimbra-stage-license.eng.zimbra.com/zimbraLicensePortal/QA/LKManager?InstallType=regular&AccountsLimit=-1&ver=2.5"
+su - zimbra -c "zmlicense -i /tmp/regular.xml && zmlicense -a"
+if  [ $? != 0 ]; then
+        echo "Error occured while installing/activating license. Kindly install/activate the license manually. "
+        exit -1
+fi
 echo "Setting LDAP passwords"
 su - zimbra -c "zmldappasswd -a test123; zmldappasswd -b test123; zmldappasswd -l test123; zmldappasswd -n test123; zmldappasswd -p test123; zmldappasswd -r test123; zmldappasswd zimbra"
 echo "Restarting mailbox"
