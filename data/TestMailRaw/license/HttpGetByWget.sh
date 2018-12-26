9010ade389319c12bdf5749ad4bc58b8ceef0ed3
SYNBUF\swapnil.pingle <swapnil.pingle@synacor.com>
2018-12-26 13:49:48 +0530

    ISA-635: zm-soap-harness leaks valid qa test licenses

--- data/TestMailRaw/license/HttpGetByWget.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-wget -O regular.xml http://zimbra-stage-license-vip.vmware.com/zimbraLicensePortal/QA/LKManager --post-data="AccountsLimit=-1&ArchivingAccountsLimit=-1&AttachmentIndexingAccountsLimit=-1&ISyncAccountsLimit=-1&MAPIConnectorAccountsLimit=-1&MobileSyncAccountsLimit=-1&SMIMEAccountsLimit=-1&InstallType=regular" --no-proxy
