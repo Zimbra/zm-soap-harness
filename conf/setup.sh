9b69e9150cedb47650a5ba98447ceedbd34924d5
zimsuchitgupta <suchit.gupta@synacor.com>
2022-04-26 11:56:51 +0530

    ZCS-11169: Added back file

--- conf/setup.sh
+++ conf/setup.sh
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 if [ "$(whoami)" != "root" ]; then
         echo "Script must be run as user: root"
         exit -1
@@ -13,4 +14,23 @@ if  [ $? != 0 ]; then
 fi
 su - zimbra -c "zmldappasswd -a test123; zmldappasswd -b test123; zmldappasswd -l test123; zmldappasswd -n test123; zmldappasswd -p test123; zmldappasswd -r test123; zmldappasswd zimbra"
 echo "Restarting mailbox"
+=======
+if [ "$(whoami)" != "root" ]; then
+        echo "Script must be run as user: root"
+        exit -1
+fi
+echo "Enabling unauth pings"
+su - zimbra -c "zmlocalconfig -e allow_unauthed_ping=true"
+echo "Installing and activating license"
+wget --no-check-certificate --no-proxy -O /tmp/regular.xml "http://zimbra-stage-license.eng.zimbra.com/zimbraLicensePortal/QA/LKManager?InstallType=regular&AccountsLimit=-1&ver=2.5"
+su - zimbra -c "zmlicense -i /tmp/regular.xml && zmlicense -a"
+if  [ $? != 0 ]; then
+        echo "Error occured while installing/activating license. Kindly install/activate the license manually. "
+        exit -1
+fi
+
+password="test123"
+su - zimbra -c "zmldappasswd -a ${password}; zmldappasswd -b ${password}; zmldappasswd -l ${password}; zmldappasswd -n ${password}; zmldappasswd -p ${password}; zmldappasswd -r ${password}; zmldappasswd zimbra"
+echo "Restarting mailbox"
+>>>>>>> e53fc4f... ZCS-11169: Added back file
 su - zimbra -c "zmmailboxdctl restart"
