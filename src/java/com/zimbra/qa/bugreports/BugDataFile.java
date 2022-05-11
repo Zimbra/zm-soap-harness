b3563153103a1b6b392f8f2de47b7b8629d51886
abhishek <abhishekdeshmukh.com@gmail.com>
2022-05-11 16:22:50 +0530

    ZCS-11169 : Worked on review comments for indentation

--- src/java/com/zimbra/qa/bugreports/BugDataFile.java
+++ src/java/com/zimbra/qa/bugreports/BugDataFile.java
@@ -7,58 +7,53 @@ import java.util.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
 /**
  * A class to help locate the folder containing the Bug Reports
+ * 
  * @author zimbra
  *
  */
 public abstract class BugDataFile {
-	private static final Logger mLogger = LogManager.getLogger(BugStatus.class);
+    private static final Logger mLogger = LogManager.getLogger(BugStatus.class);
 
-	
-	/**
-	 * A list of directory paths that can contain the bug report information
-	 */
-	private static final List<File> paths = new ArrayList<File>() {
-		private static final long serialVersionUID = 8573313916224770228L;
-	{
-	    add(new File( "/opt/qa/testlogs/BugReports" )); // Unix path
-	    add(new File( "/opt/qa/BugReports" )); 			// Unix path
-	    add(new File( "/qa/selenium/BugReports" )); 	// Unix path
-	    add(new File( "T:\\BugReports" )); 				// Windows (TMS) path
-	    add(new File( "C:\\BugReports" ));				// Windows (Dev) path
-	    add(new File( "data/BugReports/bugzilla" ));	// Eclipse (Dev) path
-	}};
-	
-	
+    /**
+     * A list of directory paths that can contain the bug report information
+     */
+    private static final List<File> paths = new ArrayList<File>() {
+        private static final long serialVersionUID = 8573313916224770228L;
+        {
+            add(new File("/opt/qa/testlogs/BugReports")); // Unix path
+            add(new File("/opt/qa/BugReports")); // Unix path
+            add(new File("/qa/selenium/BugReports")); // Unix path
+            add(new File("T:\\BugReports")); // Windows (TMS) path
+            add(new File("C:\\BugReports")); // Windows (Dev) path
+            add(new File("data/BugReports/bugzilla")); // Eclipse (Dev) path
+        }
+    };
 
-	protected BugDataFile() {
-		mLogger.info("new " + BugDataFile.class.getCanonicalName());
-		
-	}
+    protected BugDataFile() {
+        mLogger.info("new " + BugDataFile.class.getCanonicalName());
+    }
 
+    /**
+     * Return the database file, if it exists, in the normal data paths
+     * 
+     * @param filename
+     * @return
+     * @throws IOException
+     * @throws IOException
+     * @throws NoSuchAlgorithmException
+     */
+    protected File getDatafile(String filename) throws FileNotFoundException {
 
-	
-	/**
-	 * Return the database file, if it exists, in the normal data paths 
-	 * @param filename
-	 * @return
-	 * @throws IOException 
-	 * @throws IOException 
-	 * @throws NoSuchAlgorithmException 
-	 */
-	protected File getDatafile(String filename) throws FileNotFoundException {
-		
-		// Find where the database files are located
-		for (File directory : paths) {
-			File file = new File(directory, filename);
-			if ( file.exists() ) {
-				return (file);
-			}
-		}
-		
-		throw new FileNotFoundException("Unable to locate "+ filename +" in "+ Arrays.toString(paths.toArray()));
-	}
-	
+        // Find where the database files are located
+        for (File directory : paths) {
+            File file = new File(directory, filename);
+            if (file.exists()) {
+                return (file);
+            }
+        }
+
+        throw new FileNotFoundException("Unable to locate " + filename + " in " + Arrays.toString(paths.toArray()));
+    }
 }
