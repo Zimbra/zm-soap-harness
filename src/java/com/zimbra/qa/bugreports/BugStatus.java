a3f0eb5bc9be521e48076a82166cc1f993f3c9c2
abhishek <abhishekdeshmukh.com@gmail.com>
2022-05-11 18:31:49 +0530

    ZCS-11169 : Correcting indentation in bugreports folder

--- src/java/com/zimbra/qa/bugreports/BugStatus.java
+++ src/java/com/zimbra/qa/bugreports/BugStatus.java
@@ -6,9 +6,8 @@ import java.util.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
 public class BugStatus extends BugDataFile {
-	private static final Logger mLogger = LogManager.getLogger(BugStatus.class);
+    private static final Logger mLogger = LogManager.getLogger(BugStatus.class);
 
 	public enum BugState {
 		UNCONFIRMED,
@@ -20,53 +19,54 @@ public class BugStatus extends BugDataFile {
 		VERIFIED,
 		CLOSED
 	}
-	
-	/**
-	 * Return the current list of "Bug ID" to "Bug Status"
-	 * @return
-	 * @throws IOException
-	 */
-	public static Map<String, BugState> getStatusData() throws IOException {
-		BugStatus engine = new BugStatus();
-		return (engine.getData());
-	}
-	
-	protected static final String DataFilename = "bugStatus.txt";
-	
-	protected BugStatus() {
-		mLogger.info("new " + BugStatus.class.getCanonicalName());
-	}
 
-	protected Map<String, BugState> getData() throws IOException {
-		
-		// New datafile was found.  Clear the map
-		Map<String, BugState> bugStatusMap = new HashMap<String, BugState>();
-		
-		// Read the file and build the map
-		BufferedReader reader = null;
-		String line;
-		
-               try {
-                   reader = new BufferedReader(new FileReader(getDatafile(DataFilename)));
-                   while ((line = reader.readLine()) != null) {
-                       // Example: 50208 RESOLVED
-                       String[] values = line.split("\\s");
-                       if (values.length != 2) {
-                           mLogger.warn("bugStatus: invalid line: " + line);
-                           continue;
-                       }
-                       String bugid = values[0];
-                       BugState bugState = BugState.valueOf(values[1]);
-                       bugStatusMap.put(bugid, bugState);
-                       mLogger.debug("bugStatus: put " + line);
-                   }
-               } finally {
-                   if (reader != null) {
-                       reader.close();
-                       reader = null;
-                   }
-               }
-		return (bugStatusMap);
-	}
+    /**
+     * Return the current list of "Bug ID" to "Bug Status"
+     * 
+     * @return
+     * @throws IOException
+     */
+    public static Map<String, BugState> getStatusData() throws IOException {
+        BugStatus engine = new BugStatus();
+        return (engine.getData());
+    }
+
+    protected static final String DataFilename = "bugStatus.txt";
+
+    protected BugStatus() {
+        mLogger.info("new " + BugStatus.class.getCanonicalName());
+    }
+
+    protected Map<String, BugState> getData() throws IOException {
+
+        // New datafile was found. Clear the map
+        Map<String, BugState> bugStatusMap = new HashMap<String, BugState>();
+
+        // Read the file and build the map
+        BufferedReader reader = null;
+        String line;
+
+        try {
+            reader = new BufferedReader(new FileReader(getDatafile(DataFilename)));
+            while ((line = reader.readLine()) != null) {
+                // Example: 50208 RESOLVED
+                String[] values = line.split("\\s");
+                if (values.length != 2) {
+                    mLogger.warn("bugStatus: invalid line: " + line);
+                    continue;
+                }
+                String bugid = values[0];
+                BugState bugState = BugState.valueOf(values[1]);
+                bugStatusMap.put(bugid, bugState);
+                mLogger.debug("bugStatus: put " + line);
+            }
+        } finally {
+            if (reader != null) {
+                reader.close();
+                reader = null;
+            }
+        }
+        return (bugStatusMap);
+    }
 	
 }
--- src/java/com/zimbra/qa/bugreports/TestCaseEngineTestNG.java
+++ src/java/com/zimbra/qa/bugreports/TestCaseEngineTestNG.java
@@ -7,62 +7,54 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.dom4j.*;
 
-
 public class TestCaseEngineTestNG extends TestCaseEngine {
-	private static Logger mLogger = LogManager.getLogger(TestCaseEngineTestNG.class);
+    private static Logger mLogger = LogManager.getLogger(TestCaseEngineTestNG.class);
 
+    public TestCaseEngineTestNG(File results) {
+        super(results);
+        mLogger.info("new " + TestCaseEngineTestNG.class.getCanonicalName());
 
+    }
 
+    @Override
+    public List<TestCaseResult> getData() throws UnsupportedEncodingException, IOException, DocumentException {
 
-	public TestCaseEngineTestNG(File results) {
-		super(results);
-		mLogger.info("new "+ TestCaseEngineTestNG.class.getCanonicalName());
+        List<TestCaseResult> results = new ArrayList<TestCaseResult>();
+        List<String> sResults = new ArrayList<String>();
 
-	}
+        // Open the testng-results.xml file and convert to Element
+        Document document = this.getResultsDocument();
+        for (Object i : document.getRootElement().selectNodes("//groups/group/method")) {
+            if (i instanceof Element) {
+                Element eMethod = (Element) i;
 
+                String clazz = eMethod.attributeValue("class", "undefined");
+                String method = eMethod.attributeValue("name", "undefined");
 
-	@Override
-	public List<TestCaseResult> getData() throws UnsupportedEncodingException, IOException, DocumentException 
-	{
+                mLogger.info("processing: " + clazz + "." + method);
 
-		List<TestCaseResult> results = new ArrayList<TestCaseResult>();
-		List<String> sResults = new ArrayList<String>();
+                for (Object j : document.getRootElement()
+                        .selectNodes("//class[@name='" + clazz + "']/test-method[@name='" + method + "']")) {
+                    if (j instanceof Element) {
+                        Element eTestmethod = (Element) j;
 
+                        String description = eTestmethod.attributeValue("description", "undefined");
+                        String status = eTestmethod.attributeValue("status", "FAIL");
 
-		// Open the testng-results.xml file and convert to Element
-		Document document = this.getResultsDocument();
-		for ( Object i : document.getRootElement().selectNodes("//groups/group/method") ) {
-			if ( i instanceof Element ) {
-				Element eMethod = (Element)i;
-				
-				String clazz = eMethod.attributeValue("class", "undefined");
-				String method = eMethod.attributeValue("name", "undefined");
+                        mLogger.info(String.format("processed: testcase %s, description %s, status %s",
+                                clazz + "." + method, description, status));
 
-				mLogger.info("processing: "+ clazz +"."+ method);
-
-				for ( Object j : document.getRootElement().selectNodes("//class[@name='"+ clazz +"']/test-method[@name='"+ method+"']") ) {
-					if ( j instanceof Element ) {
-						Element eTestmethod = (Element)j;
-						
-						String description = eTestmethod.attributeValue("description", "undefined");
-						String status = eTestmethod.attributeValue("status", "FAIL");
-
-						mLogger.info(String.format("processed: testcase %s, description %s, status %s",
-								clazz + "." + method, description, status));
-
-						// To avoid duplicate entries as in soapws harness one test case can be part more than one group.
-						if(!sResults.contains(clazz + "." + method))	{
-							sResults.add(clazz + "." + method);
-							results.add(new TestCaseResult(clazz + "." + method, status));
-						}
-					}
-				}
-			}
-		}
-		sResults = null;
-		return (results);
-	}
-
-
-
+                        // To avoid duplicate entries as in soapws harness one test case can be part
+                        // more than one group.
+                        if (!sResults.contains(clazz + "." + method)) {
+                            sResults.add(clazz + "." + method);
+                            results.add(new TestCaseResult(clazz + "." + method, status));
+                        }
+                    }
+                }
+            }
+        }
+        sResults = null;
+        return (results);
+    }
 }
